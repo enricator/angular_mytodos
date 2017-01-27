7a51e6a95d743b1259d772b6bfa0deeec1de7dc4
Enrico Imbalzano <io@enricator.it>
2017-01-27 21:58:20 +0100

    fixed build bug - changed ngmin with ngAnnotate

--- Gruntfile.js
+++ Gruntfile.js
@@ -242,7 +242,8 @@ module.exports = function (grunt) {
     // ngmin tries to make the code safe for minification automatically by
     // using the Angular long form for dependency injection. It doesn't work on
     // things like resolve or inject so those have to be done manually.
-    ngmin: {
+    // ngmin is DEPRECATED: using ngAnnotate instead
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -377,11 +378,11 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    //'ngmin',
+    'ngAnnotate',
     'copy:dist',
-    //'cdnify',
+    'cdnify',
     'cssmin',
-    //'uglify',
+    'uglify',
     'rev',
     'usemin',
     'htmlmin'
